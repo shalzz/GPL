6c31240ff269a64b0a0ee7e288dfc7fd5ea5226f
Shalzz <shaleen.jain95@gmail.com>
2013-04-17 18:20:34 +0530

    Changed "look and feel" code

--- src/Javapackage/StartPage.java
+++ src/Javapackage/StartPage.java
@@ -24,8 +24,6 @@ import Classes.md5hash;
 import Classes.myQueries;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
 
 /*
  * To change this template, choose Tools | Templates
@@ -47,17 +45,7 @@ public class StartPage extends javax.swing.JFrame {
     public StartPage() {
         initComponents();
         
-       try {
-    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) 
-    {
-        if ("Nimbus".equals(info.getName())) {
-            UIManager.setLookAndFeel(info.getClassName());
-            break;
-        }
-    }
-} catch (Exception e) {
-    
-}
+      
     }
 
     /** This method is called from within the constructor to
@@ -234,6 +222,28 @@ public class StartPage extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
